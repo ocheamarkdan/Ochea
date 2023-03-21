e537ffb97f0431b4a59aa05bab3ab04fe5cb8b1f
Dan <dandanochea@gmail.com>
2023-03-21 23:05:04 +0800

    newlatest

---
+++ app/src/main/java/com/example/myapplicationdan/FourthActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplicationdan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+        Button Btn = (Button) findViewById(R.id.button3);
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FourthActivity.this,FifthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
